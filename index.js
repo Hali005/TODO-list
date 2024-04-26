05b47545bb4da56c3b1fff8395e69d2eb93ffd39
Hassan Ali <hassanashraf884@gmail.com>
2024-04-26 08:37:23 +0500

    first initial

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = 'true';
+while (condition) {
+    let addtask = await inquirer.prompt([
+        {
+            name: "todo",
+            message: "What you want to add in todo list?",
+            type: "input"
+        },
+        {
+            name: "addmore",
+            message: "Do you want to add more?",
+            type: "confirm",
+            default: "false"
+        }
+    ]);
+    todos.push(addtask.todo);
+    condition = addtask.addmore;
+    console.log(todos);
+}
